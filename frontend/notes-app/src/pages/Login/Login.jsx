e6489d78396ffbf40e91de3e982316182f90996e
susbnr21 <susbnr9@gmail.com>
2024-08-30 11:41:17 -0500

    Login UI Design and error handling

--- frontend/notes-app/src/pages/Login/Login.jsx
+++ frontend/notes-app/src/pages/Login/Login.jsx
@@ -1,8 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+import { Link } from 'react-router-dom'
+import PasswordInput from '../../components/Input/PasswordInput'
+import { validateEmail } from '../../utils/helper'
 
 const Login = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPaswword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    if(!validateEmail(email)) {
+      setError('Please enter a valid email address.');
+      return;
+    }
+
+    if( !password ) {
+      setError('Please enter the password')
+      return;
+    }
+
+    setError("")
+
+    //Login API Call
+  };
+
   return (
-    <div>Login</div>
+    <>
+     <Navbar />
+
+     <div className='flex items-center justify-center mt-28'>
+      <div className='w-96 border rounded bg-white px-7 py-10'>
+        <form onSubmit={handleLogin}>
+          <h4 className='text-2xl mb-7'>Login</h4>
+
+          <input 
+            type='text'
+            placeholder='Email'
+            className='input-box'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <PasswordInput
+            value={password}
+            onChange={(e) => setPaswword(e.target.value)}
+          />
+
+          {error && <p className='text-red-500 text-xs pb-1'>{error}</p>}
+          
+          <button type='submit' className='btn-primary'>Login</button>
+          
+          <p className='text-sm text-center mt-4'>
+            Not Registered Yet? {" "} <Link to="/signup" className='font-mnedium text-primary underline'>Create an Account</Link>
+          </p>
+        </form>
+      </div>
+     </div>
+    </>
   )
 }
 
